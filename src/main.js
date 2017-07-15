c6f83000ee9019f763b7df398b074c5c4f6dc2f4
Mikhail Korotkikh <mishakor23@gmail.com>
2017-07-15 23:47:25 +0300

    fix lint errors

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import Vue from 'vue';
-import Vuetify from 'vuetify'
+import Vuetify from 'vuetify';
 import App from './App';
 import router from './router';
 
-Vue.use(Vuetify)
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
