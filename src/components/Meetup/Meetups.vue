64773ff6cbd6f8831786b60d79c7f8d0f79d55f0
Mikhail Korotkikh <mishakor23@gmail.com>
2017-07-25 11:23:10 +0300

    add layout for displaying meetups list

--- src/components/Meetup/Meetups.vue
+++ src/components/Meetup/Meetups.vue
@@ -1,7 +1,36 @@
 <template lang="html">
-  <div class="">
-    <p>Meetups page</p>
-  </div>
+  <v-container>
+    <v-layout row wrap>
+      <v-flex xs12 sm10 md8 offset-sm1 offset-md2>
+        <v-card class="info">
+          <v-container fluid>
+            <v-layout row>
+              <v-flex xs5 sm4 md3>
+                <v-card-media
+                src="https://static.tonkosti.ru/tonkosti/table_img/s20/c4c4/99959938.jpg"
+                height="130px">
+                </v-card-media>
+              </v-flex>
+              <v-flex xs7 sm8 md9>
+                <v-card-title primary-title>
+                  <div class="">
+                    <h5 class="white--text mb-0">AbraCadabra</h5>
+                    <div>17th July 2017</div>
+                  </div>
+                </v-card-title>
+                <v-card-actions>
+                  <v-btn to="/meetups/1">
+                    <v-icon left light>arrow_forward</v-icon>
+                    View Meetup
+                  </v-btn>
+                </v-card-actions>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
