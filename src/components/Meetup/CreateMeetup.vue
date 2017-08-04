709f376f80b70b8dfe680db60a25dd521a0898d0
Mikhail Korotkikh <mishakor23@gmail.com>
2017-08-04 10:41:22 +0300

    add 'create meetup' from

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -1,11 +1,108 @@
 <template lang="html">
-  <div class="">
-    <p>Create Meetups page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4>Create a new Meetup</h4>
+      </v-flex>
+    </v-layout row>
+    <v-layout>
+      <v-flex xs12>
+        <form @submit.prevent="onCreateMeetup">
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="title"
+                label="Title"
+                id="title"
+                v-model="title"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="location"
+                label="Location"
+                id="location"
+                v-model="location"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="imageUrl"
+                label="Image URL"
+                id="image-url"
+                v-model="imageUrl"
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageUrl" alt="" height="200">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field
+                name="description"
+                label="Description"
+                id="idescription"
+                v-model="description"
+                multi-line
+                required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn
+                type="submit"
+                class="primary"
+                :disabled="!formIsValid">Create meetup</v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      title: '',
+      location: '',
+      imageUrl: '',
+      description: '',
+    };
+  },
+  computed: {
+    formIsValid() {
+      return this.title !== '' && this.location !== '' && this.imageUrl !== '' && this.description !== '';
+    },
+  },
+  methods: {
+    onCreateMeetup() {
+      if (!this.formIsValid) {
+        return;
+      }
+      const meetupData = {
+        title: this.title,
+        location: this.location,
+        imageUrl: this.imageUrl,
+        description: this.description,
+        date: new Date(),
+      };
+      this.$store.dispatch('createMeetup', meetupData);
+      this.$router.push('/meetups');
+    },
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -16,8 +16,24 @@ const store = new Vuex.Store({
       registeredMeetups: ['ghsdfuygsd234'],
     },
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    createMeetup(state, payload) {
+      state.loadedMeetups.push(payload);
+    },
+  },
+  actions: {
+    createMeetup({ commit }, payload) {
+      const meetup = {
+        title: payload.title,
+        location: payload.location,
+        imageUrl: payload.imageUrl,
+        description: payload.description,
+        date: payload.date,
+        id: 'asdfasdfasdf234',
+      };
+      commit('createMeetup', meetup);
+    },
+  },
   getters: {
     loadedMeetups(state) {
       return state.loadedMeetups.sort((meetupA, meetupB) => {
