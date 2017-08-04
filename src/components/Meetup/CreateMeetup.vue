2d18e72ce96f19afd4c2c6a01502a55e2f20b35d
Mikhail Korotkikh <mishakor23@gmail.com>
2017-08-04 11:12:40 +0300

    add date and time picker for create meetup form

--- src/components/Meetup/CreateMeetup.vue
+++ src/components/Meetup/CreateMeetup.vue
@@ -60,10 +60,28 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
+              <h4>Choose a Date $ Time</h4>
+            </v-flex>
+          </v-layout>
+          <v-layout row class="mb-2">
+            <v-flex xs12 sm6 offset-sm3>
+              <v-date-picker v-model="date"></v-date-picker>
+              <p>{{ date }}</p>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-time-picker v-model="time" format="24hr"></v-time-picker>
+              <p>{{time}}</p>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
               <v-btn
                 type="submit"
                 class="primary"
                 :disabled="!formIsValid">Create meetup</v-btn>
+                {{submittableDateTime}}
             </v-flex>
           </v-layout>
         </form>
@@ -80,11 +98,26 @@ export default {
       location: '',
       imageUrl: '',
       description: '',
+      date: new Date(),
+      time: new Date(),
     };
   },
   computed: {
     formIsValid() {
       return this.title !== '' && this.location !== '' && this.imageUrl !== '' && this.description !== '';
+    },
+    submittableDateTime() {
+      const date = new Date(this.date);
+      if (typeof this.time === 'string') {
+        const hours = this.time.match(/^(\d+)/)[1];
+        const minutes = this.time.match(/:(\d+)/)[1];
+        date.setHours(hours);
+        date.setMinutes(minutes);
+      } else {
+        date.setHours(this.time.getHours());
+        date.setMinutes(this.time.getMinutes());
+      }
+      return date;
     },
   },
   methods: {
@@ -97,7 +130,7 @@ export default {
         location: this.location,
         imageUrl: this.imageUrl,
         description: this.description,
-        date: new Date(),
+        date: this.submittableDateTime,
       };
       this.$store.dispatch('createMeetup', meetupData);
       this.$router.push('/meetups');
